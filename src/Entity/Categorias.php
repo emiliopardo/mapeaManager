a8a8e8d8e0fbe857beaff860869bae16fe423b56
epardo <e.pardo.perez@gmail.com>
2019-08-12 13:21:13 +0200

    se crea la entidad categoria para poder gestionar categorias de mapas

---
+++ src/Entity/Categorias.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoriasRepository")
+ */
+class Categorias
+{
+    /**
+     * @var \Ramsey\Uuid\UuidInterface The user identifier
+     * 
+     * @ORM\Id()
+     * @ORM\Column(type="uuid",unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
